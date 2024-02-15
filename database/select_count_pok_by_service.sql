d3a4032299bd66d380bc620b97a58dc41f6aab5b
Danspers <soda870@gmail.com>
2024-02-15 20:51:48 +0300

    add basic function

--- database/select_count_pok_by_service.sql
+++ database/select_count_pok_by_service.sql
@@ -1,12 +1,18 @@
--- №№ л/с квартир и row_id (внешний ключ)
---SELECT row_id, number AS acc FROM stack.accounts WHERE type = 3;
-
-SELECT accounts.number AS acc, service, COUNT(meter.value)
-FROM stack.meter_pok AS meter
-LEFT JOIN stack.accounts as accounts ON meter.acc_id = accounts.row_id
-LEFT JOIN stack.counters as counters ON meter.counter_id = counters.row_id
-WHERE date < '20230201'
-  AND counter_id IN (SELECT row_id
-                     FROM stack.counters
-                     WHERE service = 300)
-GROUP BY acc, service;
+CREATE OR REPLACE FUNCTION stack.select_count_pok_by_service(_service int, _date date)
+RETURNS TABLE(acc integer, service integer, count bigint)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT accounts.number AS acc,
+           counters.service,
+           COUNT(meter.value)
+    FROM stack.meter_pok AS meter
+    LEFT JOIN stack.accounts as accounts ON meter.acc_id = accounts.row_id
+    LEFT JOIN stack.counters as counters ON meter.counter_id = counters.row_id
+    WHERE meter.date < _date
+      AND meter.counter_id IN (SELECT row_id
+                               FROM stack.counters
+                               WHERE counters.service = _service)
+    GROUP BY accounts.number, counters.service;
+END;
+$$ language plpgsql;
