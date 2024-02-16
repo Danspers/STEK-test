a001b553bf70cde7acf77a8716b5dfbf4fdaa8c7
Danspers <soda870@gmail.com>
2024-02-16 15:33:11 +0300

    refactoring

--- database/select_count_pok_by_service.sql
+++ database/select_count_pok_by_service.sql
@@ -1,18 +1,16 @@
-CREATE OR REPLACE FUNCTION stack.select_count_pok_by_service(_service int, _date date)
+CREATE OR REPLACE FUNCTION stack.select_count_pok_by_service(_service integer, _date date)
 RETURNS TABLE(acc integer, service integer, count bigint)
 AS $$
 BEGIN
     RETURN QUERY
-    SELECT accounts.number AS acc,
-           counters.service,
-           COUNT(meter.value)
-    FROM stack.meter_pok AS meter
-    LEFT JOIN stack.accounts as accounts ON meter.acc_id = accounts.row_id
-    LEFT JOIN stack.counters as counters ON meter.counter_id = counters.row_id
-    WHERE meter.date < _date
-      AND meter.counter_id IN (SELECT row_id
-                               FROM stack.counters
-                               WHERE counters.service = _service)
-    GROUP BY accounts.number, counters.service;
+    SELECT a.number, c.service, COUNT(mp.value)
+    FROM stack.meter_pok AS mp
+    LEFT JOIN stack.accounts as a ON mp.acc_id=a.row_id
+    LEFT JOIN stack.counters as c ON mp.counter_id=c.row_id
+    WHERE mp.date < _date
+      AND mp.counter_id IN (SELECT row_id
+                            FROM stack.counters
+                            WHERE c.service=_service)
+    GROUP BY a.number, c.service;
 END;
 $$ language plpgsql;
